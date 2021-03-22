0c444ddeae5be3f0155ee26fb2f49d0be6a0f920
Thomas <lefoux82@gmail.com>
2021-03-22 15:40:24 +0100

    correction import and provider of products

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/products_providers.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
 // final String title;
@@ -14,7 +14,7 @@ static const routeName = '/detail-du-produit';
     //extraire id
     final productId = ModalRoute.of(context).settings.arguments as String;
     //appel au fournisseur recuperation des propriétés pars son id
-    final loadedProduct = Provider.of<ProductsProviders>(context, listen: false,).findById(productId);
+    final loadedProduct = Provider.of<Products>(context, listen: false,).findById(productId);
 
     return Scaffold(
       appBar: AppBar(
