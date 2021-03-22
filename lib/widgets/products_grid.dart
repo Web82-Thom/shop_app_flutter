5523c7ffa56c3ef8e122017ee76b3427b6006c1f
Thomas <lefoux82@gmail.com>
2021-03-22 15:36:31 +0100

    input ChangeNotifierProvider for provider and comment instancie products

--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/products_providers.dart';
+import '../providers/products.dart';
 import './product_item.dart';
 
 class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<ProductsProviders>(context);
+    final productsData = Provider.of<Products>(context);
     final products = productsData.items;
+    // print(products.length);
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
-      itemBuilder: (ctx, i) => ProductItem(
-        products[i].id,
-        products[i].title,
-        products[i].imageUrl,
-        products[i].price,
-       ),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c) => products[i],
+        child: ProductItem(
+          // products[i].id,
+          // products[i].title,
+          // products[i].imageUrl,
+          // products[i].price,
+        ),
+      ),
       itemCount: products.length,
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount (
         childAspectRatio: 3 / 2,
