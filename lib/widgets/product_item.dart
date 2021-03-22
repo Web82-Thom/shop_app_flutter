da6f981b8e99549c002761dff83a27de7bf9c4db
Thomas <lefoux82@gmail.com>
2021-03-22 10:01:12 +0100

    add gestureDetector, onTap, Navigator/productDetailScreen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/product_detail_screen.dart';
+
 class ProductItem extends StatelessWidget {
   //propriété pour afficher un produit
   final String id;
@@ -14,9 +16,17 @@ class ProductItem extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector( 
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routName,
+              arguments: id,
+            );
+          },
+          child: Image.network(
+            imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         header: GridTileBar(
           title: Text(
