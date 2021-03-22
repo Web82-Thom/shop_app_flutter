a0e8637f1ac02ef40cbc79f88b809c1695fd072d
Thomas <lefoux82@gmail.com>
2021-03-22 15:38:12 +0100

    replace  in comment for provider product, and instancie for element

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
+import '../providers/product.dart';
+
 
 class ProductItem extends StatelessWidget {
-  //propriété pour afficher un produit
-  final String id;
-  final String title;
-  final String imageUrl;
-  final double price;
+  // propriété pour afficher un produit
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  // final double price;
 
-  ProductItem(this.id, this.title, this.imageUrl, this.price);
+  // ProductItem(this.id, this.title, this.imageUrl, this.price);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: true);
+    // print(products[1.toInt()]);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -20,17 +25,22 @@ class ProductItem extends StatelessWidget {
           onTap: () {
             Navigator.of(context).pushNamed(
               ProductDetailScreen.routeName,
-              arguments: id,
+              // arguments: product.id,
+              arguments: product.id,
             );
           },
           child: Image.network(
-            imageUrl,
+          //  product.imageUrl,
+          //  product.imageUrl,
+           product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         header: GridTileBar(
           title: Text(
-            title,
+            // product.title,
+            product.title,
+            // product.title,
             textAlign: TextAlign.center,
           ),
         ),
@@ -38,15 +48,16 @@ class ProductItem extends StatelessWidget {
           backgroundColor: Colors.black38,
           leading: IconButton(
             icon: Icon(
-              Icons.favorite,
+              product.isFavorite?
+              Icons.favorite: Icons.favorite_border,
             ),
             onPressed: () {
-              /////
+              product.toggleFavoriteStatus();
             },
             color: Theme.of(context).accentColor,
           ),
           title: Text(
-            price.toString() + ' €',
+            product.price.toString() + ' €',
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
@@ -54,7 +65,7 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
             ),
             onPressed: () {
-              //...
+              // product.toggle
             },
             color: Theme.of(context).accentColor,
           ),
