e2c633062721e755b54db480fc31dd4e899edffd
Thomas <lefoux82@gmail.com>
2021-03-23 14:39:52 +0100

    add route for cart_screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -4,6 +4,7 @@ import 'package:provider/provider.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import '../screens/cart_screen.dart';
 
 //enum pour selectionner les fav ou tous les articles
 enum FilterOptions {
@@ -58,7 +59,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                 Icons.shopping_cart,
               ),
               onPressed: () {
-                // cart.addItem();
+                Navigator.of(context).pushNamed(CartScreen.routeName,);
               },
             ),
           ),
