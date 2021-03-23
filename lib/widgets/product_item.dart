0b01a29c9d30da5b61e417b55fe515d44e2d0cac
Thomas <lefoux82@gmail.com>
2021-03-23 13:09:38 +0100

    import provider cart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
-
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // propriété pour afficher un produit
@@ -16,7 +16,8 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context, listen: true);
+    final product = Provider.of<Product>(context, listen: false,);
+    final cart = Provider.of<Cart>(context, listen: false,);
     // print(products[1.toInt()]);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
@@ -65,7 +66,7 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
             ),
             onPressed: () {
-              // 
+             cart.addItem(product.id, product.price, product.title,);
             },
             color: Theme.of(context).accentColor,
           ),
