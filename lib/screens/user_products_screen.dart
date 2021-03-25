31ab6fb2e4687f1a5743629ed7efd152e6bcf57f
Thomas <lefoux82@gmail.com>
2021-03-25 15:24:50 +0100

    create screen for user_products

---
+++ lib/screens/user_products_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../widgets/app_drawer.dart';
+import '../providers/products.dart';
+import '../widgets/user_products_item.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/gestions-des-articles';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Gestions des produits'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      drawer: AppDrawer(),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+          itemCount: productsData.items.length,
+          itemBuilder: (_, i) => Column(
+            children: [
+              UserProductsItem(
+                productsData.items[i].title,
+                productsData.items[i].imageUrl,
+              ),
+              Divider(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
