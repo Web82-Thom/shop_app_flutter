09878dfe80c77c7995cf4f42c545bfaf4d92e3ed
Thomas <lefoux82@gmail.com>
2021-03-25 17:48:31 +0100

    add in form description, images url, and focus

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -3,14 +3,44 @@ import 'package:flutter/material.dart';
 import '../widgets/app_drawer.dart';
 
 class EditProductScreen extends StatefulWidget {
-static const routeName = '/edit-product';
+  static const routeName = '/edit-product';
 
   @override
   _EditProductScreenState createState() => _EditProductScreenState();
 }
 
 class _EditProductScreenState extends State<EditProductScreen> {
+  // ajouter un noeud en memoire
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _imageUrlFocusNode = FocusNode();
+  
+  @override
+  void initState() {
+    _imageUrlFocusNode.addListener(_updateImageUrl);
+    super.initState();
+  }
+  
+  //effacer les neoud
+  @override
+  void dispose() {
+    _imageUrlFocusNode.removeListener(_updateImageUrl);
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    _imageUrlController.dispose();
+    _imageUrlFocusNode.dispose();
+    super.dispose();
+  }
+
+  void _updateImageUrl() {
+    if(!_imageUrlFocusNode.hasFocus) {
+      setState(() {
+        
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,28 +50,69 @@ class _EditProductScreenState extends State<EditProductScreen> {
       drawer: AppDrawer(),
       body: Padding(
         padding: EdgeInsets.all(16.0),
-          child: Form(
-            child: ListView(
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Titre'),
-                  textInputAction: TextInputAction.next,
-                  onFieldSubmitted: (_) {
-                    FocusScope.of(context).requestFocus(
-                      _priceFocusNode,
-                    );
-                  },
-                ),
-                TextFormField(
-                  decoration: InputDecoration(labelText: 'Price'),
-                  textInputAction: TextInputAction.next,
-                  keyboardType: TextInputType.number,
-                  focusNode: _priceFocusNode,
-                ),
-              ],
-            ),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Titre'),
+                textInputAction: TextInputAction.next,
+                // permet de changer de zone du formulaire automatiquement
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(
+                    _priceFocusNode,
+                  );
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.only(
+                      top: 8,
+                      right: 10,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    child: _imageUrlController.text.isEmpty
+                    ? Text('Entrer une URL')
+                    : FittedBox(
+                        child: Image.network(_imageUrlController.text),
+                        fit: BoxFit.cover,
+                    ),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      decoration: InputDecoration(labelText: 'Image URL'),
+                      keyboardType: TextInputType.url,
+                      textInputAction: TextInputAction.done,
+                      controller: _imageUrlController,
+                      focusNode: _imageUrlFocusNode,
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
-      ) ,
+        ),
+      ),
     );
   }
 }
