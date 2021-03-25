e1c9900d8e6581bebf8d76c563f1557bf870f2d0
Thomas <lefoux82@gmail.com>
2021-03-25 13:42:05 +0100

    add scaffoldMessenger for undo add item in card

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -66,11 +66,32 @@ class ProductItem extends StatelessWidget {
               Icons.shopping_cart,
             ),
             onPressed: () {
-             cart.addItem(product.id, product.price, product.title,);
+              cart.addItem(
+                product.id,
+                product.price, 
+                product.title,
+              );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Produit ajouter au panier',
+                    textAlign: TextAlign.center,
+                  ), 
+                  duration: Duration(
+                    seconds: 2,
+                  ),
+                  action: SnackBarAction(
+                    label: 'Annuler',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
           ),
-          
         ),
       ),
     );
