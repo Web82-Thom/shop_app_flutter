bbd1f0e1e61b62c968adb13c70091dc61bb2bc31
Thomas <lefoux82@gmail.com>
2021-03-26 13:36:04 +0100

    add route for update product, and element Id

--- lib/widgets/user_products_item.dart
+++ lib/widgets/user_products_item.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
+import '../screens/edit_product_screen.dart';
+
 class UserProductsItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
-  UserProductsItem(this.title, this.imageUrl);
+  UserProductsItem(this.id, this.title, this.imageUrl);
   
   @override
   Widget build(BuildContext context) {
@@ -18,12 +21,15 @@ class UserProductsItem extends StatelessWidget {
         child: Row(
           children: <Widget>[
             IconButton(icon: Icon(Icons.edit), onPressed: () {
-
+              Navigator.of(context).pushNamed(
+                EditProductScreen.routeName, 
+                arguments: id,
+              );
             },
             color: Theme.of(context).primaryColor,
             ),
             IconButton(icon: Icon(Icons.delete), onPressed: () {
-
+              
             },
             color: Theme.of(context).errorColor,
             )
