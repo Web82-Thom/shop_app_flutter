47c56cc15fbae6e6b5f4907a1f30194c59db9a3a
Thomas <lefoux82@gmail.com>
2021-03-29 15:13:52 +0200

    add init and loading products, and circularProgressIndicator

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app_flutter/providers/products.dart';
 
 import '../widgets/app_drawer.dart';
 import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
+import '../providers/products.dart';
 import '../screens/cart_screen.dart';
 
 //enum pour selectionner les fav ou tous les articles
@@ -20,6 +22,36 @@ class ProductsOverviewScreen extends StatefulWidget {
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   var _showOnlyFavorites = false;
+  var _isInit = true;
+  var _isLoading = true;
+
+  @override
+  void initState() {
+    // Provider.of<Products>(context).fetchAndSetProducts();
+    // Future.delayed(Duration.zero).then((_) {
+    //   Provider.of<Products>(context).fetchAndSetProducts();
+    // });
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (_isInit) {
+      setState(() {
+       _isLoading = true;
+      });
+      Provider.of<Products>(context).fetchAndSetProducts().then((_) {
+        setState(() {
+        _isLoading = false;
+        });
+      });
+    } 
+    _isInit = false;
+    super.didChangeDependencies();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -67,7 +99,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: _isLoading 
+      ? Center(child: CircularProgressIndicator(),)
+      : ProductsGrid(_showOnlyFavorites),
     );
   }
 }
