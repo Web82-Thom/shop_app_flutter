c7025ab958de3e463e5e6adfeb0e807f2f0d4249
Thomas <lefoux82@gmail.com>
2021-03-29 10:38:55 +0200

    add provider for delete product

--- lib/widgets/user_products_item.dart
+++ lib/widgets/user_products_item.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductsItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
 
-  UserProductsItem(this.id, this.title, this.imageUrl);
+  UserProductsItem(this.id, this.title, this.imageUrl,);
   
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,7 @@ class UserProductsItem extends StatelessWidget {
             color: Theme.of(context).primaryColor,
             ),
             IconButton(icon: Icon(Icons.delete), onPressed: () {
-              
+              Provider.of<Products>(context, listen: false).deleteProduct(id);
             },
             color: Theme.of(context).errorColor,
             )
